f4ce38e2fd1421bb1b096143a0c635593ae99179
Thomas Sherwood <tsherwood99@gmail.com>
2019-09-13 21:27:05 -0400

    Created the basic app structure along with the home screen
    Basic app structure has the navigation bar with the "settings", "home" and "add" buttons. These buttons point to the respective fragments. The home screen also has the "I Don't Know What I Want" button along with the "I Know What I Want" search bar.

---
+++ app/src/main/java/com/cse442/createteamname/ui/add/AddFragment.java
@@ -0,0 +1,35 @@
+package com.cse442.createteamname.ui.add;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.cse442.createteamname.R;
+
+public class AddFragment extends Fragment {
+
+    private AddViewModel addViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        addViewModel =
+                ViewModelProviders.of(this).get(AddViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_add, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        addViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/cse442/createteamname/ui/home/HomeViewModel.java
@@ -0,0 +1,10 @@
+package com.cse442.createteamname.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    public HomeViewModel() {
+    }
+}
