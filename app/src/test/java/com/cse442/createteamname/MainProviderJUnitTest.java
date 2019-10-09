a88ef7cdb3495e4bfd33d1073ecf93a949a584e8
Zachary Ross <zjross@buffalo.edu>
2019-10-09 18:45:18 -0400

    #35 Set-Up ContentProvider Classes
    Added 2 classes with TODOs, 1 content provider, 1 test for content
    provider functions.

---
+++ app/src/test/java/com/cse442/createteamname/MainProviderJUnitTest.java
@@ -0,0 +1,27 @@
+package com.cse442.createteamname;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+import com.cse442.createteamname.MainProvider;
+
+public class MainProviderJUnitTest {
+
+    @Test
+    public void insertTest() {
+        // TODO: Create test for insert method of content provider
+        assertTrue(false);
+    }
+
+    @Test
+    public void queryTest() {
+        // TODO: Create test for query method of content provider
+        assertTrue(false);
+    }
+}
