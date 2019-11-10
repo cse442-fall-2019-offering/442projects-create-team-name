801746bcc6608bd7c1f79da5618be486bc3a9099
Thomas Sherwood <tsherwood99@gmail.com>
2019-11-10 17:37:47 -0500

    #70 Tags PHP query has ability to query tagas associated with given restaurant

--- api/tags/query.php
+++ api/tags/query.php
@@ -3,7 +3,15 @@ require "../conn.php";
 
 $sql = "SELECT tag From Tags";
 
+if (isset($_GET['rest_id'])){
+    $sql = $sql . " WHERE id IN (SELECT tag_id FROM Restaurant_Tags WHERE restaurant_id = ?)";
+}
+
 if ($stmt = mysqli_prepare($conn, $sql)) {
+    if(isset($_GET['rest_id'])){
+        mysqli_stmt_bind_param($stmt,'i',$_GET['rest_id']);
+    }
+
     mysqli_stmt_execute($stmt);
 
     if ($result = mysqli_stmt_get_result($stmt)) {
